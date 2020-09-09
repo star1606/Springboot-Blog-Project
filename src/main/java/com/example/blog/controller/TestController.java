a343a2f15da67b91367d951ce02677166400ca3d
Lee <lee@nate.com>
2020-09-09 23:55:59 +0900

    기본 test response body 데이터 요청 성공

---
+++ src/main/java/com/example/blog/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.example.blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.blog.model.Test;
+
+@Controller
+public class TestController {
+
+	@Autowired
+	Test t;
+	
+	
+	@GetMapping({"", "/"})
+	public @ResponseBody String index() {
+		
+		System.out.println(t.age);
+		System.out.println(t.money);
+		System.out.println(t.getMoney());
+		return "Hello world";
+	}
+	
+}
---
+++ src/main/java/com/example/blog/model/Test.java
@@ -0,0 +1,18 @@
+package com.example.blog.model;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Test {
+	private int id;
+	private String name;
+	public int age = 30;
+	private int money = 10000;
+}
