9004a181b8df778010654f35c5b602607e666566
Lee <you@example.com>
2020-09-10 16:25:01 +0900

    viewresolver, jackson

--- src/main/java/com/example/blog/controller/TestController.java
+++ src/main/java/com/example/blog/controller/TestController.java
@@ -17,8 +17,13 @@ public class TestController {
 	@GetMapping({"", "/"})
 	public @ResponseBody String index() {
 		
+		// => /viewresolver/src/main/resources/templates/index.mustache
+		// viewresolver의 역할 : return하는 것은 파일, prefix와 surfix를 붙여준다. return에 index만 써도 알아서 붙여준다는 뜻
+		// 원래 requestDispatcher를 써서 데이터를 보냈는데 스프링에서는 클래스로 Model 이라는것을 만들어놨음
+		// Model model  함수안에 파라미터를 적기만하면 DI해줌..  
+		
 		System.out.println(t.age);
-		System.out.println(t.money);
+//		System.out.println(t.money);
 		System.out.println(t.getMoney());
 		return "Hello world";
 	}
--- src/main/java/com/example/blog/model/Test.java
+++ src/main/java/com/example/blog/model/Test.java
@@ -2,6 +2,7 @@ package com.example.blog.model;
 
 import org.springframework.stereotype.Component;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
