e3202487d285f5722d656fda2ed130beb7f6be0f
Lee <swiniee@naver.com>
2020-09-13 00:21:36 +0900

    user model 생성, myBatis mapper user.xml, dataConfig

--- src/main/java/com/example/blog/controller/TestController.java
+++ src/main/java/com/example/blog/controller/TestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.blog.model.Test;
@@ -29,4 +30,9 @@ public class TestController {
 		return "test";
 	}
 	
+	
+	@PostMapping("/auth/joinForm")
+	public String joinForm() {
+		return "user/joinForm";
+	}
 }
